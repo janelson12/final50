b278adbba33b570235a73439d3264185a7798697
janelson12 <julietnelson13@gmail.com>
2014-12-06 23:08:40 -0500

    Update sms.py

--- alerts/sms.py
+++ alerts/sms.py
@@ -1,9 +1,13 @@
+#imports configuration settings 
 from django.conf import settings
 
+#imports twillio API
 from twilio.rest import TwilioRestClient
  
+ #Identifies my twilio account to take $$ from :( 
 client = TwilioRestClient(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
  
+ # function to send text from twillio (to is the number to be alerted, body is the message of the text)
 def send_sms(to, body):
     return client.messages.create(body=body,
     	to=to,   
