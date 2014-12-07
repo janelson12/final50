485eb52f928f98eba6eaa09ac8f237212321d352
janelson12 <julietnelson13@gmail.com>
2014-12-06 23:04:12 -0500

    Update forms.py

--- alerts/forms.py
+++ alerts/forms.py
@@ -1,17 +1,24 @@
+# django makes these cool templates FOR YOU ALREADY!!!!!! (this is me importing them)
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
+# I made this cool alert model FOR ME ALREADY (this is me importing it)
 from alerts.models import Subscriber
 
+# Now I will do magic and edit the django form to be better b/c i'm awesome
+# I'm changing the model form to base it's fields off of my subscriber model... except user (that's set automatically)
 class SubscriberForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(SubscriberForm, self).__init__(*args, **kwargs)
+        
+        # dont have shit after labels in html for this lil' form
         self.label_suffix = ''
-
+    
     class Meta:
         model = Subscriber
         exclude = ('user',)
 
+# same as above but for user 
 class UserForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(UserForm, self).__init__(*args, **kwargs)
